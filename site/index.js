4c0cedb1985a88250b75dcbd5a1f881428b9ada2
Plet53 <Plet53@thegame.com>
2026-06-23 18:11:00 -0700

    css color in svg

--- site/index.js
+++ site/index.js
@@ -1,13 +1,18 @@
 const emailTemplate = '<form>contact: <input type="submit" value="Click to reveal"></input></form';
-const copySVG = `
-  <svg
-    viewbox="0 0 60 60"
 
+function svgTag(color) {
+  return `<svg
+    viewbox="0 0 55 55"
+    width="1em"
+    style="vertical-align: center;"
   >
-    <rect x="5" y="20" width="30" height="30" fill="none" stroke-width="3" stroke="#000000"/>
-    <path d="M20 15 v-10 h30 v30 h-10" fill="none" stroke-width="3" stroke="#000000"/>
+    <rect x="5" y="20" width="30" height="30" fill="none" stroke-width="3" stroke="${color}"/>
+    <path d="M20 15 v-10 h30 v30 h-10" fill="none" stroke-width="3" stroke="${color}"/>
   </svg>
 `;
+}
+
+const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
 class EmailRevealComponent extends HTMLElement {
   connectedCallback() {
@@ -26,9 +31,14 @@ class CopyButtonComponent extends HTMLElement {
   connectedCallback() {
     if (this.querySelector('svg')) return;
 
-    this.innerHTML = copySVG;
+    darkQuery.onchange = (ev) => {
+      this.innerHTML = svgTag(getComputedStyle(this).getPropertyValue("--textcolor"));
+    }
+
+    this.innerHTML = svgTag(getComputedStyle(this).getPropertyValue("--textcolor"));
     this.onclick = async (e) => {
       await navigator.clipboard.writeText('andrewreade53@gmail.com');
+      this.innerHTML = svgTag(getComputedStyle(this).getPropertyValue("--greencolor"));
     }
   }
 }
